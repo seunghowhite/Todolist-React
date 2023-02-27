008ad5d39ac1be17c95b0df197e5f0685f0e190e
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-02-27 10:49:28 +0900

    제목 내용 추가 가능, 삭재기능 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  const [toDoList, setToDoList] = ([
-    { id: 1, title: '제목임1', content: '비어있음1' },
-    { id: 2, title: '제목임2', content: '비어있음3' },
-    { id: 3, title: '제목임2', content: '비어있음3' },
+
+  const [toDoList, setToDoList] = useState([//useState안씀
+    { id: 1, title: '제목임1', content: '비어있음1', done: 0 },
+    { id: 2, title: '제목임2', content: '비어있음3', done: 0 },
+    { id: 3, title: '제목임2', content: '비어있음3', done: 0 },
   ])
+
+
+
   const [toDoTitle, setToDoTitle] = useState('')
   const [toDoContent, setToDoContent] = useState('')
 
@@ -17,24 +21,39 @@ function App() {
     const newtodolist = {
       id: toDoList.length + 1,
       title: toDoTitle,
-      content: toDoContent
+      content: toDoContent,
+      done: 0
     }
-    setToDoList(...toDoList, newtodolist)
+    setToDoList([...toDoList, newtodolist])//멥 꼭 만들어주기
   }
+
+  const deleteToDO = (id) => {
+    setToDoList(toDoList.filter((list) => list.id !== id))
+  }
+
+
   return (
     <div>
       <div>
         제목<input value={toDoTitle} onChange={titleHander} />
         내용<input value={toDoContent} onChange={contentHander} />
+
         <button onClick={addToDO}>추가</button>
+
       </div>
       <div>
+        {toDoList.map((list) => {
+          return (
+            <div>
+              제목:{list.title}내용:{list.content}
+              <button onClick={() => deleteToDO(list.id)}>삭재</button>
 
-        <div>제목:{toDoList.title}내용:{toDoList.content}</div>
-
-
+            </div>
+          )
+        })}
       </div>
-    </div>
+      <div></div>
+    </div >
   )
 }
 
